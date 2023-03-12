c84a5ee6185ffaffe0821d466cab85357555080f
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2023-03-12 11:22:45 +0530

    recent changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-primary">
         Hello world!
       </h1>
     </div>
