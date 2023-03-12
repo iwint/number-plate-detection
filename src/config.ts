d0a526bd6520821231905339e72d0033665165f9
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2023-03-12 16:01:22 +0530

    get template

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-export const localStorageDarkModeKey = 'darkMode'
-
-export const localStorageStyleKey = 'style'
-
-export const containerMaxW = 'xl:max-w-6xl xl:mx-auto'
-
-export const appTitle = 'Free Tailwind 3 React Next Typescript dashboard template'
-
-export const getPageTitle = (currentPageTitle: string) => `${currentPageTitle} — ${appTitle}`
